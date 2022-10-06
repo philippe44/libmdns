fff91878f5ec83f008ddfeccd0b89a12991e48de
philippe44 <philippe44@users.noreply.github.com>
2022-10-05 23:46:49 -0700

    add some libraries

---
+++ build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+list="x86_64-linux-gnu-gcc x86-linux-gnu-gcc arm-linux-gnueabi-gcc aarch64-linux-gnu-gcc sparc64-linux-gnu-gcc mips-linux-gnu-gcc powerpc-linux-gnu-gcc"
+declare -A alias=( [x86-linux-gnu-gcc]=i686-linux-gnu-gcc )
+declare -A cflags=( [sparc64-linux-gnu-gcc]="-mcpu=v7" [mips-linux-gnu-gcc]="-march=mips32" [powerpc-linux-gnu-gcc]="-m32")
+declare -a compilers
+
+IFS= read -ra candidates <<< "$list"
+
+# do we have "clean" somewhere in parameters (assuming no compiler has "clean" in it...
+if [[ $@[*]} =~ clean ]]; then
+	clean="clean"
+fi	
+
+# first select platforms/compilers
+for cc in ${candidates[@]}
+do
+	# check compiler first
+	if ! command -v ${alias[$cc]:-$cc} &> /dev/null; then
+		continue
+	fi
+	
+	if [[ $# == 0 || ($# == 1 && -n $clean) ]]; then
+		compilers+=($cc)
+		continue
+	fi
+
+	for arg in $@
+	do
+		if [[ $cc =~ $arg ]]; then 
+			compilers+=($cc)
+		fi
+	done
+done
+
+declare -a items=(mdns-sd tinysvcmdns)
+
+# then iterate selected platforms/compilers
+for item in ${items[@]}
+do
+	for cc in ${compilers[@]}
+	do
+		IFS=- read -r platform host dummy <<< $cc
+	
+		export CFLAGS=${cflags[$cc]}
+	
+		pwd=$(pwd)
+		cd $item
+		make CC=${alias[$cc]:-$cc} PLATFORM=$platform $clean
+		if [[ -n $clean ]]; then
+			continue
+		fi
+	
+		mkdir -p ../targets/$host/$platform
+		cp lib/$host/$platform/lib*.a $_		
+		ar -rc --thin $_/libmdns.a $_/lib*.a
+		cd $pwd
+	done	
+done

e0fd3cb50deb97d16a3a3a5113756bee8ec5c3ed
philippe44 <philippe44@users.noreply.github.com>
2022-10-06 22:45:25 -0700

    updated build

--- build.cmd
+++ build.cmd
@@ -2,12 +2,15 @@ setlocal
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
 
-msbuild libmdns.sln /property:Configuration=Debug
-msbuild libmdns.sln /property:Configuration=Release
+msbuild libmdns.sln /property:Configuration=Debug %1
+msbuild libmdns.sln /property:Configuration=Release %1
 
 set target=targets\win32\x86
 
-del %target%\*.lib
+if exist %target% (
+	del %target%\*.lib
+)
+
 robocopy lib\win32\x86 %target% lib*.lib lib*.pdb /NDL /NJH /NJS /nc /ns /np
 robocopy tinysvcmdns targets\include\tinysvcmdns tinysvcmdns.h /NDL /NJH /NJS /nc /ns /np
 robocopy mdnssd targets\include\mdnssd mdnssd.h /NDL /NJH /NJS /nc /ns /np

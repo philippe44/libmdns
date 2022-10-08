9bf3ad4e94656d9539b8a8af11512c444fdae2fa
philippe44 <philippe44@users.noreply.github.com>
2022-10-07 22:01:45 -0700

    add windows debug library

--- build.cmd
+++ build.cmd
@@ -15,6 +15,7 @@ robocopy lib\win32\x86 %target% lib*.lib lib*.pdb /NDL /NJH /NJS /nc /ns /np
 robocopy tinysvcmdns targets\include\tinysvcmdns tinysvcmdns.h /NDL /NJH /NJS /nc /ns /np
 robocopy mdnssd targets\include\mdnssd mdnssd.h /NDL /NJH /NJS /nc /ns /np
 lib.exe /OUT:%target%/libmdns.lib %target%/libtinysvcmdns.lib %target%/libmdnssd.lib
+lib.exe /OUT:%target%/libmdnsd.lib %target%/libtinysvcmdnsd.lib %target%/libmdnssdd.lib
 
 endlocal
 
